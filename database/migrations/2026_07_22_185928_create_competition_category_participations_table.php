0cca7f90095ee2ca6d206daa175dc644858dee37
FazalDinBidec <fazal@bidecsol.com>
2026-07-24 01:13:54 +0500

    Update 2026_07_22_185928_create_competition_category_participations_table.php

--- database/migrations/2026_07_22_185928_create_competition_category_participations_table.php
+++ database/migrations/2026_07_22_185928_create_competition_category_participations_table.php
@@ -13,11 +13,26 @@ return new class extends Migration
     {
         Schema::create('competition_category_participations', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('competition_category_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('participation_type_id')->constrained()->cascadeOnDelete();
+
+            $table->foreignId('competition_category_id');
+            $table->foreignId('participation_type_id');
+
+            $table->foreign('competition_category_id', 'ccp_category_fk')
+                ->references('id')
+                ->on('competition_categories')
+                ->cascadeOnDelete();
+
+            $table->foreign('participation_type_id', 'ccp_participation_fk')
+                ->references('id')
+                ->on('participation_types')
+                ->cascadeOnDelete();
+
             $table->timestamps();
 
-            $table->unique(['competition_category_id', 'participation_type_id'], 'category_participation_unique');
+            $table->unique(
+                ['competition_category_id', 'participation_type_id'],
+                'category_participation_unique'
+            );
         });
     }
 
